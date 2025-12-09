ad5d39a8986e34614c422fee10767a128a82d73c
Ricardo Castro <ricardo.caroj99@gmail.com>
2025-12-09 05:50:43 -0600

    feat: se crean los validators con Joi + Celebrate

---
+++ backend/middleware/validation.js
@@ -0,0 +1,34 @@
+const { celebrate, Joi, Segments } = require('celebrate');
+
+// eslint-disable-next-line operator-linebreak
+const urlPattern =
+  /^(https?:\/\/)([\w\-.]+)\.([a-z]{2,})([\w\-._~:/?#[\]@!$&'()*+,;=.]+)?$/i;
+
+module.exports.validateCreateUser = celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(urlPattern),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(8).required(),
+  }),
+});
+
+module.exports.validateUpdateProfile = celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    about: Joi.string().min(2).max(30).required(),
+  }),
+});
+
+module.exports.validateUpdateAvatar = celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    avatar: Joi.string().pattern(urlPattern).required(),
+  }),
+});
+
+module.exports.validateUserId = celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    userId: Joi.string().hex().length(24).required(),
+  }),
+});
